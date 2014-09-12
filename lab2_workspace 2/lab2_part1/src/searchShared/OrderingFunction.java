eaa7e6e51f777a5e0c20f056ab48501175c0ac45
dit3637 <harrison.bornstein@absg.com>
2014-09-12 08:57:23 +0200

    First Commit

---
+++ lab2_workspace 2/lab2_part1/src/searchShared/OrderingFunction.java
@@ -0,0 +1,16 @@
+package searchShared;
+
+import java.util.ArrayList;
+
+
+public class OrderingFunction {
+
+    public void addNodeToQueue(SearchNode node, NodeQueue q) {
+        throw new RuntimeException("should be implemented by subclass");
+    }
+
+	public void addNodesToQueue(ArrayList<SearchNode> wrapStatesInNodes, NodeQueue frontier) {
+		// TODO Auto-generated method stub
+		
+	}
+}
